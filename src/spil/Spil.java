316da3310fe6912fe671c7f641bce7bdfb161396
Khurrrmiiii <Khurrrmiiii@khurram-macbook.lan>
2016-10-10 23:47:42 +0200

    Simpelt A+B=C

--- src/spil/Spil.java
+++ src/spil/Spil.java
@@ -4,7 +4,10 @@ public class Spil {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int A = 3;
+		int B = 2;
+		int C = A+B;
+		System.out.println(C);
 	}
 
 }
