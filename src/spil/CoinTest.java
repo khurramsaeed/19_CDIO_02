a99fae9d3ecc6a0ca4d1312f983926e05683eb1d
taygunboran <taygunboran@tayguns-air.clients.wireless.dtu.dk>
2016-10-31 14:42:58 +0100

    Coin test added

---
+++ src/spil/CoinTest.java
@@ -0,0 +1,17 @@
+package spil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoinTest {
+
+	@Test
+	public void Cointest() {
+	Coin test = new Coin();
+	int result = test.getFeltEffect();
+	assertEquals(1000, result);
+	
+	}
+
+}
